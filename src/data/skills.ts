474cc56d9fdd28ade0a4b59feb3528816123bb68
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-05-14 19:50:02 +0530

    skills data added !!

---
+++ src/data/skills.ts
@@ -0,0 +1,42 @@
+export const skills = [
+  {
+    title: "Frontend",
+    items: [
+      "React",
+      "TypeScript",
+      "JavaScript",
+      "Tailwind CSS",
+      "HTML",
+      "CSS",
+    ],
+  },
+
+  {
+    title: "Backend",
+    items: [
+      "Node.js",
+      "Express.js",
+      "REST APIs",
+      "JWT Auth",
+    ],
+  },
+
+  {
+    title: "Database",
+    items: [
+      "MongoDB",
+      "Mongoose",
+      "Firebase",
+    ],
+  },
+
+  {
+    title: "Tools",
+    items: [
+      "Git",
+      "GitHub",
+      "Postman",
+      "VS Code",
+    ],
+  },
+];
